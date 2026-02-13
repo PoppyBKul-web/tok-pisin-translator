4592381c47dba2e11bf6c08fadc726fae5f9e404
PoppyBKul-web <olekapriestly@gmail.com>
2026-02-14 00:59:05 +1000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function myFunction() { 
+document.getElementById("demo1").innerHTML = "Yu bilong wanem hap bilong kantri?"; 
+document.getElementById("demo2").innerHTML = "Yu stap long [nau ples] inap hamas yia pinis?"; 
+document.getElementById("demo5").innerHTML = "Yu bin raun long ol narapela kantri pinis?"; 
+document.getElementById("demo6").innerHTML = "Yu save yusim wanem kain mani?"; 
+document.getElementById("demo7").innerHTML = "Wanem nem bilong yu?"; document.getElementById("demo8").innerHTML = "Wanem tok ples yupela olgeta sa tok tok?"; 
+} 
